20655a2cddf218471be5a03f7d1b84d229490aec
SUSINDHAR.S <susindhar.s2006@gmail.com>
2026-04-21 11:00:28 +0530

    new addition

---
+++ templates/app.py
@@ -0,0 +1,22 @@
+from flask import Flask, render_template, request, redirect
+
+app = Flask(__name__)
+
+students = []
+
+@app.route('/')
+def index():
+    return render_template("index.html", students=students)
+
+@app.route('/add', methods=['POST'])
+def add():
+    name = request.form['name']
+    email = request.form['email']
+    course = request.form['course']
+
+    students.append((name, email, course))
+
+    return redirect('/')
+
+if __name__ == '__main__':
+    app.run(host="0.0.0.0", port=10000)
